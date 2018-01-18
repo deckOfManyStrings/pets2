043f7c921e05a6e88c696b44c14942a37f940473
Zachary Rosenlund <zrosenlund@mail.greenriver.edu>
2018-01-18 17:26:15 -0500

    changed order2 routing and form1 input

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  * Date: 1/18/2018
  * Time: 1:31 PM
  */
+    session_start();
 
     //Require the autoload file
     require_once('vendor/autoload.php');
@@ -22,9 +23,9 @@
     $f3->route('GET /pets/show/@pet', function($f3, $params) {
         switch($params['pet']) {
             case 'cat':
-                echo "<img src='https://static.pexels.com/photos/177809/pexels-photo-177809.jpeg' alt='cat'><br>";
+                echo "<img src='https://static.pexels.com/photos/177809/pexels-photo-177809.jpeg' alt='cat'><br>"; break;
             case 'dog':
-                echo "<img src='https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg' alt='dog'><br>";
+                echo "<img src='https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg' alt='dog'><br>"; break;
             //404 error
             default:
                 $f3->error(404);
@@ -39,9 +40,15 @@
     }
     );
 
-    $f3->route('GET /pets/order2', function ($f3, $params)
+    $f3->route('POST /pets/order2', function ()
     {
-        echo "<h3> Form 2</h3><br>";
+        $_SESSION['animal'] = $_POST['animal'];
+
+//        echo $_SESSION['animal'];
+//        echo $_POST['animal'];
+
+        $template = new Template();
+        echo $template->render('views/form2.html');
     }
     );
 
