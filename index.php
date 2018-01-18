2ac7903e64434cd69b186442f1fe6a5d3d8e71de
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-18 17:01:40 -0500

    added routes order, order2, results

--- index.php
+++ index.php
@@ -22,15 +22,35 @@
     $f3->route('GET /pets/show/@pet', function($f3, $params) {
         switch($params['pet']) {
             case 'cat':
-                echo "<img src='https://static.pexels.com/photos/177809/pexels-photo-177809.jpeg' alt='cat'>";
+                echo "<img src='https://static.pexels.com/photos/177809/pexels-photo-177809.jpeg' alt='cat'><br>";
             case 'dog':
-                echo "<img src='https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg' alt='dog'>";
+                echo "<img src='https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg' alt='dog'><br>";
             //404 error
             default:
                 $f3->error(404);
         }
     }
     );
+
+    $f3->route('GET /pets/order', function ($f3, $params)
+    {
+        echo "<h3> Form 1</h3><br>";
+    }
+    );
+
+    $f3->route('GET /pets/order2', function ($f3, $params)
+    {
+        echo "<h3> Form 2</h3><br>";
+    }
+    );
+
+    $f3->route('GET /pets/results', function ($f3, $params)
+    {
+        echo "<h3> Results</h3><br>";
+    }
+    );
+
+
 
     //Run fat free
     $f3->run();
